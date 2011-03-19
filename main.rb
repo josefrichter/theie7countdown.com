9cd7968180d6949f9326a1943a77527ca3a8a4f9
Josef Richter <josef.richter@me.com>
2011-03-19 20:06:58 +0100

    showing appropriate html based on url tag

--- main.rb
+++ main.rb
@@ -2,7 +2,13 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  erb :index
+  erb :index, :locals => { :tag => nil }
+end
+
+get '/:tag' do
+
+  erb :index, :locals => { :tag => params[:tag] }
+  
 end
 
 # TODO scrap the data from the statcounter website automatically
